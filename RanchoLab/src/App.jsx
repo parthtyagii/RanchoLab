6d7e7dc8598a40c9d28899cfc3bd47359b7b14e1
Parth Tyagi <parthtyagicode@gmail.com>
2023-06-13 20:45:12 +0530

    loader added

--- RanchoLab/src/App.jsx
+++ RanchoLab/src/App.jsx
@@ -1,15 +1,33 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Main from './Components/Main/Main';
+import { useEffect, useState } from 'react';
+import GridLoader from 'react-spinners/GridLoader';
 
 
 function App() {
 
-  return (
-    <div className="w-full h-auto bg-green-300 flex flex-col justify-start items-center ">
+  const [loader, setLoader] = useState(true);
 
-      <Navbar />
-      <Main />
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 1500);
+  }, []);
+
+  return (
+    <div className="w-full h-auto bg-green-300 absolute flex flex-col justify-start items-center ">
+
+      {loader ? (
+        <div className='w-screen h-screen flex justify-center items-center relative top-0 bg-white '>
+          <GridLoader color="#1b1464" size={20} />
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Main />
+        </>
+      )}
 
     </div>
   )
--- RanchoLab/src/Components/Navbar/Navbar.jsx
+++ RanchoLab/src/Components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
 
       <div className="w-[30%] h-[100%] flex justify-start items-center ">
-        <img className='h-[60%] border-2 ml-[2rem] border-white ' src={logo} alt="logo" />
+        <img className='h-[70%] border-2 ml-[2rem] border-white ' src={logo} alt="logo" />
       </div>
 
       <div className="w-[70%] h-[100%] text-[2rem] flex justify-start items-center ">
